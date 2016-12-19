5b6a5117f10fa0cd54d1b2ad23f4ec39e6c4a256
jamebluntcc <jamebluntxy@gmail.com>
2016-12-19 15:37:53 +0800

    add scatter and hist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #coding=utf-8
+#this a python script to generate circos plot
+# version == 0.1 heatmap hist and scatter
 import jinja2
 import os
 import configparser
@@ -45,6 +47,18 @@ class heatmap_plot(circos_plot):
     def get_scale_log_base(self):
         print "scale_log_base:%f\nheatmap_color:%s"%(self.scale_log_base,self.color)
 
+class hist_plot(circos_plot):
+    def __init__(self,fill_color):
+        self.fill_color = fill_color
+    def get_fill_color(self):
+        print "fill_color:%s"%(self.fill_color)
+
+class scatter_plot(circos_plot):
+   def __init__(self,color,glyph,glyph_size):
+       self.color = color
+       self.glyph = glyph
+       self.glyph_size = glyph_size
+
 #template = circos_env.get_template('.conf')
 if __name__ == "__main__":
     circos_data_dir = 'home/public/software/circos-0.69-3/circos-0.69-3/data/'
@@ -58,13 +72,14 @@ if __name__ == "__main__":
 
     plot_data_list = plot_data.split(',')
     value_range_list = value_range.split(',')
+    plot_track = create_track_r(len(plot_data_list))
 
     if plot_type == 'heatmap':
         heatmap_color = command.get('circos_plot','color')
         heatmap_scale_log_base = command.get('circos_plot','scale_log_base')
         heatmap_scale_log_base_list = heatmap_scale_log_base.split(',')
         heatmap_color_list = heatmap_color.split(',')
-        heatmap_track = create_track_r(len(heatmap_color_list))
+        heatmap_track = plot_track
 
         heatmap_plot_list = []
         j = 0
@@ -79,11 +94,71 @@ if __name__ == "__main__":
             heatmap_plot_list.append(tmp)
 
         template = circos_env.get_template('circos_heatmap.conf')
-        with open('test.conf','w') as f:
-            f.write(template.render(chrom_name = karyotype,
-                                    plot_type = plot_type,
-                                    plots = heatmap_plot_list,
-                                    axes = False))
+        with open('test.conf', 'w') as f:
+            f.write(template.render(chrom_name=karyotype,
+                                plot_type=plot_type,
+                                plots=heatmap_plot_list,
+                                axes=True))
+
+
+    elif plot_type == 'hist':
+        hist_fill_color = command.get('circos_plot','fill_color')
+        hist_fill_color_list = hist_fill_color.split(',')
+        hist_track = plot_track
+
+        hist_plot_list = []
+        j = 0
+        for i in range(len(plot_data_list)):
+            tmp = hist_plot(fill_color=hist_fill_color_list[i])
+            tmp.file = plot_data_list[i]
+            tmp.min = value_range_list[j]
+            tmp.max = value_range_list[j + 1]
+            tmp.r1 = hist_track[j]
+            tmp.r0 = hist_track[j + 1]
+            j = j + 2
+            hist_plot_list.append(tmp)
+
+        template = circos_env.get_template('circos_hist.conf')
+        with open('test2.conf', 'w') as f:
+                f.write(template.render(chrom_name=karyotype,
+                                        plot_type=plot_type,
+                                        plots=hist_plot_list,
+                                        hist_fill_color=False,
+                                        axes=True))
+
+    elif plot_type == 'scatter':
+        scatter_color = command.get('circos_plot', 'color')
+        scatter_glyph = command.get('circos_plot','glyph')
+        scatter_glyph_size = command.get('circos_plot','glyph_size')
+        scatter_glyph_list = scatter_glyph.split(',')
+        scatter_color_list = scatter_color.split(',')
+        scatter_glyph_size_list = scatter_glyph_size.split(',')
+        scatter_track = plot_track
+
+        scatter_plot_list = []
+        j = 0
+        for i in range(len(plot_data_list)):
+            tmp = scatter_plot(color=scatter_color_list[i],glyph=scatter_glyph_list[i],glyph_size=scatter_glyph_size_list[i])
+            tmp.file = plot_data_list[i]
+            tmp.min = value_range_list[j]
+            tmp.max = value_range_list[j + 1]
+            tmp.r1 = scatter_track[j]
+            tmp.r0 = scatter_track[j + 1]
+            j = j + 2
+            scatter_plot_list.append(tmp)
+
+        template = circos_env.get_template('circos_scatter.conf')
+        with open('test3.conf', 'w') as f:
+            f.write(template.render(chrom_name=karyotype,
+                                    plot_type=plot_type,
+                                    plots=scatter_plot_list,
+                                    axes=True))
+
+
+
+
+
+
 
 
 

27ae02c8232de9d3277cdd1d072b0e57851b8b49
jamebluntcc <291552579@qq.com>
2017-06-29 15:23:59 +0800

    add config circos_data_dir

--- __init__.py
+++ __init__.py
@@ -5,8 +5,16 @@ circos_plot's configure
 '''
 import os
 import jinja2
+import configparser
 
 base_dir = os.path.join(__file__)
+configFilePath = os.path.join(base_dir,'circos_plot.conf')
+
+command = configparser.Configparser()
+command.read(configFilePath)
+
+circos_data_dir = command.get('circos_config','circos_data_dir')
+
 circos_env = jinja2.Environment(
     trim_blocks = True,
     autoescape = False,
@@ -26,4 +34,3 @@ track_r = {'plot_1':['0.95r','0.75r'], #track_width = 0.2
            'plot_4':['0.95r','0.80r','0.75r','0.60r','0.50r','0.35r','0.30r','0.15r'],
            'plot_6':['0.99r','0.89r','0.88r','0.78r','0.68r','0.58r','0.57r','0.47r','0.37r','0.27r','0.26r','0.16r']
           }
-          

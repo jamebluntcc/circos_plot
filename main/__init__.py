6cd0871873a064a604e06455b885f7677a041239
jamebluntcc <291552579@qq.com>
2017-06-30 09:42:41 +0800

    fix bugs

--- main/__init__.py
+++ main/__init__.py
@@ -7,13 +7,13 @@ import os
 import jinja2
 import configparser
 
-base_dir = os.path.join(__file__)
+base_dir = os.path.dirname(__file__)
 configFilePath = os.path.join(base_dir,'circos_plot.conf')
 
-command = configparser.Configparser()
+command = configparser.ConfigParser()
 command.read(configFilePath)
 
-circos_data_dir = command.get('circos_config','circos_data_dir')
+circos_data_dir = command.get('circos-config','circos_data_dir')
 
 circos_env = jinja2.Environment(
     trim_blocks = True,
@@ -26,7 +26,7 @@ heatmap_color = ('brbg-3-div-rev','spectral-3-div-rev','rdbu-3-div-rev')
 circos_conf = {'out_circle':0.99,
                'paired_dist':0.01,
                'track_dist':0.05,
-               'max_track_width'：0.2}
+               'max_track_width':0.2}
 
 track_r = {'plot_1':['0.95r','0.75r'], #track_width = 0.2
            'plot_2':['0.95r','0.80r','0.65r','0.50r'], #track_width = 0.15 track_dist = 0.15
